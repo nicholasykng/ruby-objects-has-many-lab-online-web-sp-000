e1203a68cb133b93d130c70a88047645fc3850b2
Nicholas Ng <nicholasykng@gmail.com>
2019-10-29 03:05:10 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,23 +1,21 @@
 class Artist
   attr_accessor :name
-  @@song_count = 0
+  @@all = []
   def initialize(name)
     @name = name
-    @songs = []
+    @@all << self
   end
   def songs
     Song.all.select {|song| song.artist == self}
   end
 
   def add_song(song)
-    @songs << song
     song.artist = self
 
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
-    @songs << song
     song.artist = self
 
   end
