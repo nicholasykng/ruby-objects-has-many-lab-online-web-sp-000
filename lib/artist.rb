7cb16336a54c3f2acc4ed45d7da2104b8f50ad22
Nicholas Ng <nicholasykng@gmail.com>
2019-10-29 03:02:43 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -6,20 +6,20 @@ class Artist
     @songs = []
   end
   def songs
-    @songs
+    Song.all.select {|song| song.artist == self}
   end
 
   def add_song(song)
     @songs << song
     song.artist = self
-    @@song_count += 1
+    
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
-    @@song_count += 1
+    
   end
 
   def self.song_count
