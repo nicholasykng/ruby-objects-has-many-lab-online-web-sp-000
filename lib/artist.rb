93fe47333b6f43dba8572e7cae3f1face08e6a2c
Nicholas Ng <nicholasykng@gmail.com>
2019-10-29 03:02:46 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -12,14 +12,14 @@ class Artist
   def add_song(song)
     @songs << song
     song.artist = self
-    
+
   end
 
   def add_song_by_name(name)
     song = Song.new(name)
     @songs << song
     song.artist = self
-    
+
   end
 
   def self.song_count
