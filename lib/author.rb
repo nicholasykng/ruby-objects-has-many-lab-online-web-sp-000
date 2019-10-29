9abae7fffca58bcd081ed7de29515d37438a018e
Nicholas Ng <nicholasykng@gmail.com>
2019-10-29 03:09:53 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,22 +1,18 @@
 class Author
   attr_accessor :name
-  @@post_count = 0
+  @@all = []
   def initialize(name)
     @name = name
-    @posts = []
+    @@all = []
   end
   def posts
     @posts
   end
   def add_post(post)
-    @posts << post
-    @@post_count += 1
     post.author = self
   end
   def add_post_by_title(title)
     post = Post.new(title)
-    @posts << post
-    @@post_count += 1
     post.author = self
   end
 
